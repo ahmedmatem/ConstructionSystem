1cf0355a92d8c6d2572113e723e25b21d9107d58
Ahmed Ahmed <ahmedmatem@gmail.com>
2014-12-21 15:17:36 +0000

    Implement bootstrap-cosmo Implement Awesome Fonts for Web Icons

--- Source/Web/ConstructionSystem.Web/App_Start/BundleConfig.cs
+++ Source/Web/ConstructionSystem.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace ConstructionSystem.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/bootstrap.cosmo.css",
                       "~/Content/site.css"));
         }
     }
